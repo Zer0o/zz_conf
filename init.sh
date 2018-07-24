476f3f4c14eb26b20a94f6f1728d43a2358e1868
zhu <451290491@qq.com>
2018-07-24 16:35:52 +0800

    fix get vimruntime

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-#vim init begin
+# install git begin
+
+sudo yum install git -y
+
+# install git end
+
+# vim init begin
 
 vim/vim_init.sh
 
-#vim init end
+# vim init end
--- vim/vim_init.sh
+++ vim/vim_init.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 # 1. install pathogen
+echo "install pathogen begin"
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # 2. install NERDTree
 
+echo "install NERDTree begin"
+
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle
 
 # 3. install ctags
 
-sudo yum install ctags
+sudo yum install ctags -y
 
 cd `dirname $0`
 path=`pwd`
 
-vimruntime=`vim --cmd 'echo $VIMRUNTIME|q'`
+# fixme: how to get runtime another way?
+vv=`vim --version|head -n 1|awk '{print $5}'|tr -d .`
+vimruntime="/usr/share/vim/vim"${vv}
 dst_zz=${vimruntime}/colors/zz.vim
 dst_cvim=${vimruntime}/syntax/c.vim
 cvim=${path}/c.vim
